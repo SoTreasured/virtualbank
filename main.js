3d640997afa425918eb22b8832be8a81b156c4b2
Treasure <treasureihedigbo1@gmail.com>
2020-09-10 21:56:00 +0100

    you create a problem that solves another. i need that emoji with the dissapointed face rn

--- main.js
+++ main.js
@@ -1,22 +1,42 @@
 var add;
+add = 0;
 var balance;
 balance = 0;
-balance += add;
 var withdraw;
+withdraw = 0;
 
 
 function addMoney (){
     add = window.prompt("Enter amount to add");
-    alert("You have just added N" + add + " to your account.");
+
+    if (add == "" || isNaN(add)){
+        alert("Please input a valid Number");
+    }else{
+        alert("You have just added N" + add + " to your account.");
+        balance = add;
+    }
  }
     
 
  function withdrawMoney (){
-     withdraw = window.prompt("Enter amount to withdraw");
-    alert("You have just withdrawn N" + withdraw + " from your account.");
- }
+
+     balance = balance - withdraw;
+
+     if(balance == 0){
+        alert("Insufficient funds");
+     }else if (balance <= 1000){
+         alert("Sorry, your have insufficient balance to make any withdrawals.\nYour balance needs to be greater than N1,000 before any withdrawal can be made.\n\nClick on ADD to add money and start making withdrawals. ");
+     }else if(balance > withdraw){
+        alert("Sorry, your balance is not sufficient enough for such withdrawals.\nCheck your balance to know what amouunt you can withdraw ");
+     }else{
+        withdraw = window.prompt("Enter amount to withdraw");
+        alert("You have just withdrawn N" + withdraw + " from your account.");
+     }
+
+      
+    }
 
  function checkBalance (){
-     alert("You have N" + balance + "left in your account");
+     alert("You have N" + balance + " left in your account");
    
  }
