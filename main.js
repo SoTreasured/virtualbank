882e6be6533aa51d85719e01250da0fe22a2f1d6
Treasure <treasureihedigbo1@gmail.com>
2020-09-11 14:40:15 +0100

    the bank charge is working fine now but iyke still insists on a smart balance reading

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var add;
 add = 0;
-var balance;
-balance = 0;
+var balance 
+balance= 0;
 var withdraw;
 withdraw = 0;
+
 
 
 
@@ -13,33 +14,29 @@ function addMoney (){
     if (add == "" || isNaN(add)){
         alert("Please input a valid Number");
     }else{
-        balance = balance + add;
+        balance += add;
         alert("You have just added N" + add + " to your account.");
     }
  }
     
 
  function withdrawMoney (){
-
-     balance = balance - withdraw;
-
-     if(balance <= 0){
-        alert("Insufficient funds");
+    if(balance <= 0){
+        alert("Please make a deposit first");
      }else if (balance <= 1000){
          alert("Insufficient balance.\nYour balance needs to be greater than N1,000 before any withdrawal can be made.\n\nClick on ADD to add money and start making withdrawals. ");
     }else{
         withdraw =Number(window.prompt("Enter amount to withdraw"));
-        
-        
-    }if(withdraw < balance){
-        balance = balance - withdraw;
-        alert("You have just withdrawn N" + withdraw + " from your account.");
+    }if(balance - (withdraw + withdraw * 0.05) <= 1000){
+        alert("Oops!\n\nYou must have at least N1,000 in your account after every withdrawals. ");
     }else{
-        alert("You exceeded your balance of " + balance);
+        balance -= withdraw + withdraw * 0.05;
+        alert("You have just withdrawn N" + withdraw + " from your account, and a bank charge of 0.5% has been deducted from your account.");
      
      }
 
-    } 
+    }
+     
     
 
  function checkBalance (){
