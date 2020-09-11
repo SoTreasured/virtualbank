029ad9755052604f61f62e56b9167c00416f9865
Treasure <treasureihedigbo1@gmail.com>
2020-09-11 13:34:04 +0100

    Its finally working. Its just remaining the 5% deduction from every withdraw. Courtesy of Precious

--- main.js
+++ main.js
@@ -6,14 +6,15 @@ var withdraw;
 withdraw = 0;
 
 
+
 function addMoney (){
-    add = window.prompt("Enter amount to add");
+    add = Number(window.prompt("Enter amount to add"));
 
     if (add == "" || isNaN(add)){
         alert("Please input a valid Number");
     }else{
+        balance = balance + add;
         alert("You have just added N" + add + " to your account.");
-        balance = add;
     }
  }
     
@@ -22,19 +23,24 @@ function addMoney (){
 
      balance = balance - withdraw;
 
-     if(balance == 0){
+     if(balance <= 0){
         alert("Insufficient funds");
      }else if (balance <= 1000){
-         alert("Sorry, your have insufficient balance to make any withdrawals.\nYour balance needs to be greater than N1,000 before any withdrawal can be made.\n\nClick on ADD to add money and start making withdrawals. ");
-     }else if(balance > withdraw){
-        alert("Sorry, your balance is not sufficient enough for such withdrawals.\nCheck your balance to know what amouunt you can withdraw ");
-     }else{
-        withdraw = window.prompt("Enter amount to withdraw");
+         alert("Insufficient balance.\nYour balance needs to be greater than N1,000 before any withdrawal can be made.\n\nClick on ADD to add money and start making withdrawals. ");
+    }else{
+        withdraw =Number(window.prompt("Enter amount to withdraw"));
+        
+        
+    }if(withdraw < balance){
+        balance = balance - withdraw;
         alert("You have just withdrawn N" + withdraw + " from your account.");
+    }else{
+        alert("You exceeded your balance of " + balance);
+     
      }
 
-      
-    }
+    } 
+    
 
  function checkBalance (){
      alert("You have N" + balance + " left in your account");
